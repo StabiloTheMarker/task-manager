55b898b03a3e9dfbac32ff4b5eebc2ee4c540e92
Filip Jukic <filip.jukic@at.zurich.com>
2023-04-01 18:40:21 +0200

    setup i18next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import {useTranslation} from "next-i18next";
 
 
 export default function Home() {
+  const { t } = useTranslation('common')
   return (
-      <h1 className="text-2xl">Hello World</h1>
+      <h1 className="text-2xl">{t("Hallo")}</h1>
   )
 }
